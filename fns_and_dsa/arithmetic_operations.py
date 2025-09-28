168e41eb3bf8238eb2c142665d1d4447b0075a04
poseitutu10 <phinehasoseitutu1@gmail.com>
2025-09-28 18:17:59 -0400

    module_package

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation(num1,num2,operation):
+  match operation:
+    case "add":
+      sum = float(num1) + float(num2)
+      return sum
+    case "subtract":
+      subtract = float(num1) - float(num2);
+      return subtract
+    case "multiply":
+      multiply = float(num1) * float(num2);
+      return multiply
+    case "divide":
+      try:
+        divide = float(num1) / float(num2);
+        return divide
+      except:
+        print("Can't divide by zero")
+    case _:
+      print('Invalid operation')
+
+print(__name__)
