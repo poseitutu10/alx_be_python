a173a30c6320368a8a8b5d8560c4a2f51b3ccd04
poseitutu10 <phinehasoseitutu1@gmail.com>
2025-10-06 08:53:37 -0400

    programming paradigm

---
+++ programming_paradigm/main.py
@@ -0,0 +1,18 @@
+import sys;
+from robust_division_calculator import safe_divide
+
+def main():
+  if len(sys.argv) != 3:
+    print("Usage: python main.py <numerator> <denominator>");
+    sys.exit(1)
+  numerator = sys.argv[1];
+  denumerator = sys.argv[2];
+
+  result = safe_divide(numerator, denumerator)
+  if result != None:
+    print(f"The result of the division is {result}")
+  
+
+
+if __name__ == "__main__":
+  main()
---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,9 @@
+def safe_divide(numerator, denominator):
+  try: 
+    result = float(numerator) / float(denominator);
+  except ZeroDivisionError as e:
+    print("Error: Cannot divide by zero.")
+  except ValueError as e:
+    print("Error: Please enter numeric values only.")
+  else: 
+   return result
