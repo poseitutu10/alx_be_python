85946fa505c1ad25fd050885ac36a32e8ef7d85a
poseitutu10 <phinehasoseitutu1@gmail.com>
2025-10-06 07:52:31 -0400

    programming paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,15 +4,12 @@ class BankAccount:
     self.account_balance = account_balance;
   def deposit(self, amount):
     self.account_balance = self.account_balance + amount;
-    print(f'Deposited: ${amount}')
   def withdraw(self, amount):
     if self.account_balance >= amount:
       self.account_balance -= amount;
-      print(f"Withdrew: ${amount}")
       return True
     else: 
-      print(f"Insufficient funds.")
       return False
   def display_balance(self):
-      print(f"Current Balance: ${self.account_balance}")
+      print(f"Current Balance: ${float(self.account_balance)}")
 
